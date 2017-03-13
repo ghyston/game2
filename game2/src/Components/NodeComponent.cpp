831436ecc1a40f1c0c9fa925807c540d944b4074
Ilja Stepanow <hyston@Iljas-MacBook-Pro.local>
2017-03-13 08:43:33 +0100

    delete node cmp;

--- game2/src/Components/NodeComponent.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  NodeComponent.cpp
-//  Game2
-//
-//  Created by Stepanov Ilia on 08/11/13.
-//  Copyright (c) 2013 Stepanov Ilia. All rights reserved.
-//
-
-#include "../Components.h"
---
+++ kringle/src/kringle.h
@@ -0,0 +1,13 @@
+//
+//  kringle.h
+//  kringle
+//
+//  Created by Ilja Stepanow on 30/12/2016.
+//  Copyright © 2016 Ilja Stepanow. All rights reserved.
+//
+
+#ifndef kringle_h
+#define kringle_h
+
+
+#endif /* kringle_h */
